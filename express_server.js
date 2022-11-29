1bf17144090ff59132a725157fe82005c78e90de
Carol Cheung <carol.cheung@telus.com>
2022-11-28 17:58:51 -0800

    added shortId generator and redirect urls/shortId

--- express_server.js
+++ express_server.js
@@ -4,19 +4,45 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {
+  let randomString = "";
+  let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  while (randomString.length < 6) {
+    randomString += char.charAt(Math.floor(Math.random() * charLength));
+    }
+    return randomString;
+  };
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  
+  const shortId = generateRandomString()
+
+  urlDatabase[shortId] = req.body.longURL
+  
+  res.redirect(`/urls/${shortId}`); 
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/", (req, res) => {
